4e7e4c852bf969c084f95f707f42a4120a87ff2f
yuyangsun333 <yuyangsun@ufl.edu>
2025-04-19 21:37:11 -0400

    feat user-based and item based

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 import express from "express";
-import { recommendUserCF, recommendItemCF } from "./recommender/index.js";
+import cors     from "cors";
+import { recommendUserCF }  from "./recommender/userCF.js";
+import { recommendItemCF }  from "./recommender/itemCF.js";
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (_,res)=>res.send("Beauty Recommender API 🚀"));
+app.use(cors());
+app.get("/", (_,res)=>res.send("Beauty Recommender API "));
 
-app.get("/recommend/user/:uid", (req,res) => {
-    res.json(recommendUserCF(req.params.uid));
+// user-based
+app.get("/recommend/user/:uid", (req, res) => {
+    const uid = req.params.uid;
+    const list = recommendUserCF(uid, 10);        // top‑10
+    res.json({ mode:"user", uid, recommendations: list });
 });
 
-app.get("/recommend/item/:pid", (req,res) => {
-    res.json(recommendItemCF(req.params.pid));
+// item-based
+app.get("/recommend/item/:pid", (req, res) => {
+    const pid = req.params.pid;
+    const list = recommendItemCF(pid, 10);        // top‑10
+    res.json({ mode:"item", pid, recommendations: list });
 });
 
 app.listen(PORT, () => console.log(`→ http://localhost:${PORT}`));
